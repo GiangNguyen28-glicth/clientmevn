5ee118bbca88ee7671642ecf26ecf89b3b5a62a8
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-03-08 19:43:55 +0700

    up

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,3 +1,3 @@
 import SocketIO from "socket.io-client";
-const socket = SocketIO("https://servermevn.vercel.app/",{ transports : ['websocket'],autoConnect:false, upgrade: false});
+const socket = SocketIO("ws://localhost:3000",{ transports : ['websocket'],autoConnect:false, upgrade: false});
 export default socket;
